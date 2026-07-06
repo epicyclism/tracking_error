c8fd0dda226e34d2e9c62af0d4fd1dc3e95ffabb
Paul <paul@epicyclism.com>
2026-07-06 21:17:40 +0100

    Crashes and needs debugger. VSC and MacOS debug is broken....

--- src/tracking_error.cpp
+++ src/tracking_error.cpp
@@ -12,7 +12,11 @@
 constexpr double inner_min  = 54.0;
 constexpr double outer_max = 150.0;
 constexpr double scan_increment = 0.01;
-std::vector<double> x_axis;
+constexpr double rpm = 100.0 / 3;
+constexpr double pk_vel = 100.0; // mm/s
+// radius and velocity at rpm for interesting region
+std::vector<double> x_axis;         // mm
+std::vector<double> x_axis_velocity;// mm/s
 
 // assumes values cross 0, good assumption for this application.
 //
@@ -39,11 +43,20 @@ template<typename I> I find_next_opposite_sign(I b, I e)
     return b;
 }
 
+// tracking error in radians
 inline double compute_tracking_error(double pivot_spindle, double pivot_stylus, double radius)
 {
     auto d = radius * radius - pivot_spindle * pivot_spindle + pivot_stylus * pivot_stylus;
     d /= (2.0 * pivot_stylus);
-    return 360.0 * std::asin(d / radius) / (2.0 * std::numbers::pi);
+    return std::asin(d / radius);
+}
+
+//  2nd harmonic percent assuming pk recording velocity.
+// te tracking error radians, gs rotational groove vel mm/s
+// After Baerwald then Stevenson
+inline double compute_tracking_distortion( double te, double gs)
+{
+    return 100.0 * pk_vel * std::tan(te) / gs;
 }
 
 struct geometry_t
@@ -52,6 +65,7 @@ struct geometry_t
     double pivot_spindle_;
     double pivot_stylus_;
     double offset_;
+    double offset_rad_cache_;
     bool modified_;
 };
 
@@ -68,11 +82,34 @@ struct geometry_data_t
     std::vector<double> zeroes_;
 };
 
-void recompute(geometry_t const& g, geometry_data_t& data)
+inline double from_degrees(double d)
+{
+    return d * 2.0 * std::numbers::pi / 360.0;
+}
+
+inline double to_degrees(double r)
+{
+    return r * 360.0 / (2.0 * std::numbers::pi);
+}
+
+inline void update_offset_rad_cache(geometry_t& g)
+{
+    g.offset_rad_cache_ = from_degrees(g.offset_);
+}
+
+inline void recompute(geometry_t const& g, geometry_data_t& data)
 {
     data.tracking_error_.resize(size_t((outer_max - inner_min) / scan_increment));
+    data.tracking_distortion_.resize(size_t((outer_max - inner_min) / scan_increment));
     double rad = inner_min;
-	std::ranges::generate(data.tracking_error_, [&]() { auto e = compute_tracking_error(g.pivot_spindle_, g.pivot_stylus_, rad); rad += scan_increment; return e - g.offset_; });
+    for(auto i = 0; i < data.tracking_error_.size(); ++i)
+    {
+        auto e = compute_tracking_error(g.pivot_spindle_, g.pivot_stylus_, rad);
+        e -= g.offset_rad_cache_;
+        auto d = compute_tracking_distortion(e, x_axis_velocity[i]);
+        data.tracking_error_[i] = to_degrees(e);
+        data.tracking_distortion_[i] = d;
+    }
     data.zeroes_.clear();
     for(auto v = find_next_opposite_sign(data.tracking_error_.begin(), data.tracking_error_.end()); v != data.tracking_error_.end(); v = find_next_opposite_sign(v, data.tracking_error_.end()))
         data.zeroes_.emplace_back(inner_min + double(std::distance(data.tracking_error_.begin(), v)) * 0.01);
@@ -135,6 +172,7 @@ void draw(std::array<geometry_t, 6>& g, size_t current_geometry, std::array<geom
     if (ImGui::InputDouble("Headshell offset", &tmp, 0.1, 30.0, "%.2f"))
     {
         g[current_geometry].offset_ = tmp;
+        update_offset_rad_cache(g[current_geometry]);
         modded = true;
     }
     if (modded)
@@ -147,6 +185,7 @@ void draw(std::array<geometry_t, 6>& g, size_t current_geometry, std::array<geom
     {
         ImPlot::SetupAxes("radius (mm)","error (deg)");
 		ImPlot::PlotLine("Tracking Error", x_axis.data(), data[current_geometry].tracking_error_.data(), static_cast<int>(data[current_geometry].tracking_error_.size()));
+		ImPlot::PlotLine("Tracking Distortuib", x_axis.data(), data[current_geometry].tracking_distortion_.data(), static_cast<int>(data[current_geometry].tracking_error_.size()));
         ImPlot::EndPlot();
     }
     ImGui::TextUnformatted("Zeroes at ");
@@ -164,12 +203,19 @@ int main()
 	std::array<geometry_t, 6> geometries{ rega, linn, rega, linn, SME, SME12};
 	std::array<geometry_data_t, 6> geometries_data;
     size_t current_geometry = 0;
-
-    for(size_t i = 0; i < 6; ++i)
-        recompute(geometries[i], geometries_data[i]);
-    x_axis.resize(geometries_data[0].tracking_error_.size());
     auto x = inner_min;
+    // mm and cm/s ? 
     std::ranges::generate(x_axis, [&](){ auto xr = x; x += scan_increment; return xr;});    
+    std::ranges::generate(x_axis_velocity, [&](){ auto xr = x; x += scan_increment; return xr * 2 * std::numbers::pi * rpm / 60.0;});    
+    
+    for(size_t i = 0; i < 6; ++i)
+    {
+        update_offset_rad_cache(geometries[i]);
+        recompute(geometries[i], geometries_data[i]);
+    }
+    x_axis.resize(geometries_data[0].tracking_error_.size());
+    x_axis_velocity.resize(geometries_data[0].tracking_error_.size());
+ 
     HelloImGui::RunnerParams runnerParams;
     runnerParams.appWindowParams.windowTitle = "Tracking Error Evaluator";
         runnerParams.callbacks.ShowGui = [&]() {
